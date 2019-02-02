a8b403895b22d99603037109263a2f288984e74e
Delta Ink Design <izula97@gmail.com>
2019-02-02 13:11:19 +0100

    Create Template Literals.js

---
+++ Template Literals.js
@@ -0,0 +1,10 @@
+let word1 = 'Izabela';
+let word2 = 'Michalak';
+let num1 = 2;
+let num2 = 3;
+
+const fullName = `${num1 + num2} ${word2}`;
+let example = `${word1} ${word2}`;
+console.log(fullName);
+console.log(example)
+document.getElementById('example').innerText = example;

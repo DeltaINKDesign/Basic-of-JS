b5eda97469f2cd903c4ed75e2ca0cd75d6e3903a
Delta Ink Design <izula97@gmail.com>
2019-02-02 11:33:31 +0100

    Create Object.js

---
+++ Object.js
@@ -0,0 +1,10 @@
+let example1 = {
+    firstName: 'Dylan',
+    lastName: 'Israel',
+    address: {
+        city: 'Austin',
+        state: 'Texas'
+    }
+};
+
+console.log(example1.address.city);

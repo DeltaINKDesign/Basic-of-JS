cb15527e2d987401a0cd6f7c1fb104429fe61d0a
DeltaINKDesign <izula97@gmail.com>
2019-04-11 12:44:25 +0200

    add

---
+++ Zmienne/scrypt/1_zmienne.js
@@ -0,0 +1,34 @@
+//alert('sesja');
+
+
+let zimenna = 7;
+/*
+  zmienna - nazwa zmiennej
+  = operator przypisania
+  7 wartosc zmiennej
+*/
+
+let imie = 'Janusz';
+let nazwisko;
+nazwisko = 'Nowak';
+
+// + konkateacja (łączenie stringów)
+
+document.write('Twoje imie: ' + imie)
+
+let wiek = 21;
+
+console.log(wiek);
+
+let x,y;
+x = 7;
+y = 10;
+let suma = x + y;
+
+document.write('<br>Suma ', x , 'i', y , ' = ', suma);
+
+//potegowanie
+let pot = Math.pow(2,10);
+console.log('Potega: ', pot);
+
+//typy danych
